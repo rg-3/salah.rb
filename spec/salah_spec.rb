3f82f7795da758e3daf1fcdbaf7e985e08cb6b00
rg <1aab@protonmail.com>
2021-03-14 23:47:33 -0300

    add shared_examples/ directory

--- spec/salah_spec.rb
+++ spec/salah_spec.rb
@@ -1,27 +1,5 @@
 require_relative 'setup'
 
-RSpec.shared_examples 'Salah.today expectations' do
-  it "includes today's five prayers" do
-    expect(response.prayers.size).to eq(5)
-  end
-
-  it "includes a prayer name with each prayer" do
-    expect(response.prayers.map(&:name)).to eq(%w[Fajr Dhuhr Asr Maghrib Isha])
-  end
-
-  it "includes a prayer time with each prayer" do
-    expect(response.prayers.map(&:time)).to eq(prayer_times)
-  end
-
-  it 'includes an iso8601 date with each prayer' do
-    response.prayers.each {|prayer| expect(prayer.date.iso8601).to eq(iso8601) }
-  end
-
-  it 'includes a hijri date with each prayer' do
-    response.prayers.each {|prayer| expect(prayer.date.hijri).to eq(hijri) }
-  end
-end
-
 RSpec.describe Salah do
   describe '.today' do
     describe 'with the city of Rabat' do
--- spec/setup.rb
+++ spec/setup.rb
@@ -1,6 +1,7 @@
 require 'salah'
 require 'rspec'
 require 'vcr'
+require_relative 'shared_examples/salah.today'
 
 VCR.configure do |c|
   c.cassette_library_dir = File.join __dir__, 'fixtures', 'vcr_cassettes'
