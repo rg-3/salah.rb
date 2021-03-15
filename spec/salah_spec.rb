672818037cf1588fd49c2d6abf30c42c393f579b
rg <1aab@protonmail.com>
2021-03-14 23:37:01 -0300

    improve salah_spec.rb

--- spec/salah_spec.rb
+++ spec/salah_spec.rb
@@ -1,32 +1,29 @@
 require_relative 'setup'
 
+RSpec.shared_examples 'Salah.today expectations' do
+  it "includes today's five prayers" do
+    expect(response.prayers.size).to eq(5)
+  end
+
+  it "includes a prayer name with each prayer" do
+    expect(response.prayers.map(&:name)).to eq(%w[Fajr Dhuhr Asr Maghrib Isha])
+  end
+
+  it "includes a prayer time with each prayer" do
+    expect(response.prayers.map(&:time)).to eq(prayer_times)
+  end
+
+  it 'includes an iso8601 date with each prayer' do
+    response.prayers.each {|prayer| expect(prayer.date.iso8601).to eq(iso8601) }
+  end
+
+  it 'includes a hijri date with each prayer' do
+    response.prayers.each {|prayer| expect(prayer.date.hijri).to eq(hijri) }
+  end
+end
+
 RSpec.describe Salah do
-
-
-
   describe '.today' do
-    shared_examples 'shared expectations' do
-      it "includes today's five prayers" do
-        expect(response.prayers.size).to eq(5)
-      end
-
-      it "includes a prayer name with each prayer" do
-        expect(response.prayers.map(&:name)).to eq(%w[Fajr Dhuhr Asr Maghrib Isha])
-      end
-
-      it "includes a prayer time with each prayer" do
-        expect(response.prayers.map(&:time)).to eq(prayer_times)
-      end
-
-      it 'includes an iso8601 date with each prayer' do
-        response.prayers.each {|prayer| expect(prayer.date.iso8601).to eq(iso8601) }
-      end
-
-      it 'includes a hijri date with each prayer' do
-        response.prayers.each {|prayer| expect(prayer.date.hijri).to eq(hijri) }
-      end
-    end
-
     describe 'with the city of Rabat' do
       # When the VCR cassettes are rewritten to disk update
       # these three values to match.
@@ -36,7 +33,7 @@ RSpec.describe Salah do
 
       let(:response) { described_class.today(city: 'Rabat') }
       around {|ex| VCR.use_cassette('Salah.Today.Rabat') { ex.run } }
-      include_examples 'shared expectations'
+      include_examples 'Salah.today expectations'
 
       it 'includes a city with each prayer' do
         response.prayers.each {|prayer| expect(prayer.location.city).to eq('Rabat') }
@@ -56,14 +53,14 @@ RSpec.describe Salah do
 
       let(:response) { described_class.today(longitude: 39.816667, latitude: 21.416667)}
       around {|ex| VCR.use_cassette('Salah.Today.Mecca') { ex.run} }
-      include_examples 'shared expectations'
+      include_examples 'Salah.today expectations'
 
       it 'includes a country code with each prayer' do
         response.prayers.each{|prayer| expect(prayer.location.country_code).to eq('SA')}
       end
     end
 
-    describe 'with no city, coordinates or IP' do
+    describe 'with no city, coordinates or IP address' do
       let(:response) { described_class.today }
       it 'raises an error' do
         expect { response }.to raise_error(Salah::HTTP::NoLocationError)
@@ -77,7 +74,7 @@ RSpec.describe Salah do
       end
     end
 
-    describe 'with longitude but no langitude' do
+    describe 'with longitude but no latitude' do
       let(:response) { described_class.today longitude: 21.0 }
       it 'raises an error' do
         expect { response }.to raise_error(Salah::HTTP::NoLocationError)
